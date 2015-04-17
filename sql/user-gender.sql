f889f331a9558b7c843e7e238fce037896aa45e5
dpranantha <dpranantha@bol.com>
2015-04-17 15:57:20 +0200

    sql update

--- sql/user-gender.sql
+++ sql/user-gender.sql
@@ -1,2 +1,6 @@
-select id, gender from liverw.dps_user
-where rownum <= 5000
+select id as user_id, b.customer_number account_number, gender
+from liverw.dps_user a, liverw.bol_user b
+where 
+a.id = b.user_id
+and
+rownum <= 5000;
