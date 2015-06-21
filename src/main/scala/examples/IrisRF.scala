d1692f2417b1dd8f0026e9648fe73d0b2cb1f272
Arthur Vlug <avlug@bol.com>
2015-06-21 13:25:32 +0200

    Refactor: cleanup runners by adding Dataset objects

--- src/main/scala/examples/IrisRF.scala
+++ src/main/scala/examples/IrisRF.scala
@@ -5,11 +5,15 @@ import genderclassification.randomforest.RandomForestExecutor
 import org.apache.spark._
 import org.apache.spark.mllib.util.MLUtils
 
-object IrisRF extends Logging with GenderClassificationData {
-  override implicit val sc: SparkContext = new SparkContext(new SparkConf().setAppName("Iris random forest example").setMaster("local"))
+object IrisRF extends GenderClassificationData {
+  override implicit lazy val sc: SparkContext = new SparkContext(
+    new SparkConf()
+      .setAppName("Iris random forest example")
+      .setMaster("local"))
 
-  // libsvm Style iris Data - http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/multiclass/iris.scale
-  def dataset = MLUtils.loadLibSVMFile(sc, "input/iris.scale")
-
-  new RandomForestExecutor(dataset, 4)
+  def main(args: Array[String]) = new RandomForestExecutor(
+    // libsvm Style iris Data - http://www.csie.ntu.edu.tw/~cjlin/libsvmtools/datasets/multiclass/iris.scale
+    dataset = MLUtils.loadLibSVMFile(sc, "input/iris.scale"),
+    numClasses = 4)
+  .start()
 }
